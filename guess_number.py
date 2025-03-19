aa8132576410a82333c004df02c690ebab28912e
Виктор Викторов <vic411@rambler.ru>
2025-03-19 22:39:10 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -7,10 +7,11 @@ print(guess_number)
 
 while True:
     num_player = int(input('ВВедите число от 0 до 100: '))
-    if num_player == guess_number:
-        break
+    
     if num_player > guess_number:
         print('Ваше число больше того, что загадано')
-    if num_player < guess_number:
+    elif num_player < guess_number:
         print('Ваше число меньше того, что загадано')
+    elif num_player == guess_number:
+        break
 print('Отличная интуиция! Вы угадали число :')
